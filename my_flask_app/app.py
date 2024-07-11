63acbd48f3fe6c2eca3b01703387c856a52c54e9
fomin <fominykh@bk.ru>
2024-07-11 14:34:14 +0500

    add

---
+++ my_flask_app/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/sum/<int:a>/<int:b>')
+def sum(a, b):
+    res = a+b
+    return str(res)
+
+
+@app.route('/min/<int:a>/<int:b>')
+def min(a, b):
+    res = a-b
+    return str(res)
+
+
+@app.route('/mult/<int:a>/<int:b>')
+def mult(a, b):
+    res = a*b
+    return str(res)
+
+
+@app.route('/dell/<int:a>/<int:b>')
+def dell(a, b):
+    if b == 0:
+        return "Ошибка"
+
+    res = a/b
+    return str(res)
+
+
+@app.route('/sqr/<int:a>')
+def sqr(a):
+    res = a**2
+    return str(res)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8888)
